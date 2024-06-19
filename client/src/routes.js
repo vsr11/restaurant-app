81bbfacabc91345f827e5c3b0efb9464e6f4dc00
Vittorio Rossitto <vittorio.s.rossitto@gmail.com>
2024-06-19 10:55:48 +0300

    Add page not found component

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "./components/Main.js";
+import NotFound from "./components/NotFound.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <NotFound />,
   },
 ]);
 
