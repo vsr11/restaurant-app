2a36531328528ead710cc587a9a0a407a50d3644
Vittorio Rossitto <vittorio.s.rossitto@gmail.com>
2024-06-24 15:51:44 +0300

    Added server error handler

---
+++ server/errorHandler.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export default function errorHandler(err, req, res, next) {
+  if (err instanceof mongoose.MongooseError) {
+    err = Object.values(err.errors).map((x) => x.message)[2];
+  }
+
+  if (!err instanceof Error) {
+    err = new Error(err);
+  }
+
+  console.log(err);
+  return res.status(500).end(err);
+}
