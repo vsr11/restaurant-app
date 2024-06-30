26d8e59c14ed1ed1d2ca771e45b1dfc5e0bfa190
Vittorio Rossitto <vittorio.s.rossitto@gmail.com>
2024-06-30 15:16:41 +0300

    Add auth routing and controller to server

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,8 @@
 import { Router } from "express";
+import authController from "./controllers/authController.js";
+
 const router = Router();
+
+router.use("/auth", authController);
+
 export default router;
