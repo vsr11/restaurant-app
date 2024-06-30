36ad07bbd9b406798e65f41071101fddbcf9807d
Vittorio Rossitto <vittorio.s.rossitto@gmail.com>
2024-06-30 19:10:22 +0300

    Add Register component and route to client

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,3 +1,49 @@
+import { useState } from "react";
+import useFetchGeneric from "../../hooks/useFetchGeneric.js";
+import { SERVER_URL } from "../../constants.js";
+import { form2object } from "../../utils.js";
+
 export default function Register() {
-  return <h1>Register</h1>;
+  const [reg, setReg] = useState({});
+
+  useFetchGeneric(SERVER_URL + "auth/register", reg);
+
+  function onSubmitHandler(e) {
+    e.preventDefault();
+    const data = form2object(new FormData(e.target));
+    setReg(data);
+  }
+
+  return (
+    <>
+      <h1>Register</h1>
+      <form method="POST" onSubmit={onSubmitHandler}>
+        <div>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            E-mail:
+            <input type="text" name="email" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password:
+            <input type="password" name="password" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Repeat Password:
+            <input type="password" name="repeatPassword" />
+          </label>
+        </div>
+        <input type="submit" value="Register" />
+      </form>
+    </>
+  );
 }
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "./components/Main.js";
+import Register from "./components/auth/Register.js";
 import NotFound from "./components/NotFound.js";
 
 const router = createBrowserRouter([
@@ -7,6 +8,12 @@ const router = createBrowserRouter([
     path: "/",
     element: <Main />,
     errorElement: <NotFound />,
+    children: [
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
   },
 ]);
 
