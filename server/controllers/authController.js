27dc15d9a48ce710513abd956557b430e072843a
Vittorio Rossitto <vittorio.s.rossitto@gmail.com>
2024-06-30 17:11:49 +0300

    Add register route and function to server

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
+import User from "../models/User.js";
 
 const router = Router();
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const user = await User.create(req.body);
+    return res.status(201).json(user);
+  } catch (e) {
+    next(e);
+  }
+});
 
 export default router;
