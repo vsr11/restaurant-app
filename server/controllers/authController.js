b92d803a6b131236a0de5de59d63f4284a69b7c8
Vittorio Rossitto <vittorio.s.rossitto@gmail.com>
2024-07-02 15:20:31 +0300

    Exit registration early if user exists

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,8 +7,9 @@ router.post("/register", async (req, res, next) => {
   try {
     let user = await User.findOne({ email: req.body.email });
     if (user) {
-      return;
+      return res.json(null);
     }
+
     user = await User.create(req.body);
     return res.status(201).json(user);
   } catch (e) {
