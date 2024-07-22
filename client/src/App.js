baf94a69c4f2e3f7d12b16ecdea564be25ad54ca
Vittorio Rossitto <vittorio.s.rossitto@gmail.com>
2024-07-22 15:03:44 +0300

    Add Header and Footer components to router

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,12 @@ import { RouterProvider } from "react-router-dom";
 import { ErrorProvider } from "./contexts/ErrorContext.js";
 import { AuthProvider } from "./contexts/AuthContext.js";
 
-import Header from "./components/Header.js";
-import Footer from "./components/Footer.js";
-
 export default function App() {
   return (
     <div className="App">
       <AuthProvider>
         <ErrorProvider>
-          <Header />
-          <RouterProvider router={router} />
-          <Footer />
+          <RouterProvider router={router}></RouterProvider>
         </ErrorProvider>
       </AuthProvider>
     </div>
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,5 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header.js";
+import Footer from "./Footer.js";
 
 export default function Main() {
-  return <Outlet />;
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
