042f38bf4f9c199c88014a4aaa3b541716e47bc9
Vittorio Rossitto <vittorio.s.rossitto@gmail.com>
2024-07-22 15:31:48 +0300

    Add a navigation bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom";
 import Err from "./Err.js";
 
 export default function Header() {
   return (
     <>
       <h1>Header</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+        </ul>
+      </nav>
       <Err />
     </>
   );
