f8ca24fff85e7b6e1627adb3091d494431cc6e72
Vittorio Rossitto <vittorio.s.rossitto@gmail.com>
2024-07-31 14:56:42 +0300

    Add user role

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,16 @@
+import { defaults } from "../constants.js";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  role: {
+    type: String,
+    required: true,
+    enum: ["ADMIN", "USER"],
+    default: defaults.account_role,
+  },
 });
 
 export default mongoose.model("User", userSchema);
