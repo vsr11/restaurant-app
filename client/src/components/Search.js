803ae584ae666c46213aabc449852ace3dc2fa98
Vittorio Rossitto <vittorio.s.rossitto@gmail.com>
2024-08-17 10:32:46 +0300

    Add loading state to search component

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -2,6 +2,7 @@ import useFetch from "../hooks/useFetch.js";
 import { SERVER_URL } from "../constants.js";
 import { useSearchParams } from "react-router-dom";
 import { form2object } from "../utils.js";
+import Loading from "./Loading.js";
 
 export default function Search() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -11,7 +12,9 @@ export default function Search() {
     setSearchParams(form2object(e.target));
   }
 
-  const [result] = useFetch(`${SERVER_URL}/menu/search?${searchParams}`);
+  const [result, isLoading] = useFetch(
+    `${SERVER_URL}/menu/search?${searchParams}`
+  );
 
   return (
     <>
@@ -23,21 +26,22 @@ export default function Search() {
         />
         <input type="submit" value="Search" />
       </form>
-      {(result?.ok && result?.data?.length && (
-        <section id="results">
-          {result?.data?.map((x, i) => {
-            return (
-              <div key={i}>
-                <div>Name: {x?.name}</div>
-                <div>Category: {x?.category}</div>
-                <div>Description: {x?.description}</div>
-                <div>Weight: {x?.weight}</div>
-                <div>Price: {x?.price}</div>
-              </div>
-            );
-          })}
-        </section>
-      )) ||
+      {(isLoading && <Loading />) ||
+        (result?.ok && result?.data?.length && (
+          <section id="results">
+            {result?.data?.map((x, i) => {
+              return (
+                <div key={i}>
+                  <div>Name: {x?.name}</div>
+                  <div>Category: {x?.category}</div>
+                  <div>Description: {x?.description}</div>
+                  <div>Weight: {x?.weight}</div>
+                  <div>Price: {x?.price}</div>
+                </div>
+              );
+            })}
+          </section>
+        )) ||
         (Object.fromEntries(searchParams.entries())?.query && (
           <h2>Nothing found!</h2>
         ))}
