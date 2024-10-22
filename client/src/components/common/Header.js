c5add7e770b1a9d579f16750c24c725d2c8dca61
Vittorio Rossitto <vittorio.s.rossitto@gmail.com>
2024-10-22 16:19:35 +0300

    Show appropriate links in header when logged in or out

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,22 +1,33 @@
 import { Link } from "react-router-dom";
 import Err from "./Err.js";
+import { useContext } from "react";
+import AuthContext from "../../contexts/AuthContext.js";
+import "./Header.css";
 
 export default function Header() {
+  const ac = useContext(AuthContext);
+  const isLoggedIn = ac?.get();
+
   return (
     <>
       <header>
         <h1>Header</h1>
         <nav>
           <ul>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/logout">Logout</Link>
-            </li>
+            {(!isLoggedIn && (
+              <>
+                <li>
+                  <Link to="/register">Register</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </>
+            )) || (
+              <li>
+                <Link to="/logout">Logout</Link>
+              </li>
+            )}
             <li>
               <Link to="/search">Search</Link>
             </li>
